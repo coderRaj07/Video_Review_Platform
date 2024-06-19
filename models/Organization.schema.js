92e681d42ad09c86d166bb4f8dcc712220b5ed42
coderRaj07 <12131501.edu@gmail.com>
2024-06-19 19:17:59 +0530

    feat: comments added

--- models/Organization.schema.js
+++ models/Organization.schema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const organizationSchema = new Schema({
   orgName: { type: String, required: true },
-  orgOwner: { type: Schema.Types.ObjectId, ref: 'User', required: true },    // Youtuber
+  orgOwner: { type: Schema.Types.ObjectId, ref: 'User', required: true },    // Give "youtuber" role to the owner in "OrganizationMember" schema
   subscriptions: [{ type: Schema.Types.ObjectId, ref: 'Subscription' }]
 });
 module.exports = mongoose.model('Organization', organizationSchema);
