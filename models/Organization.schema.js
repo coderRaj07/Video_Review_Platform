9d3842c36546df1960cff0587d18aee430a1b1f1
coderRaj07 <12131501.edu@gmail.com>
2024-06-23 18:15:03 +0530

    feat(subscription-model): orgName should be unique

--- models/Organization.schema.js
+++ models/Organization.schema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const organizationSchema = new Schema({
-  orgName: { type: String, required: true },
+  orgName: { type: String, required: true,  unique: true },
   orgOwner: { type: Schema.Types.ObjectId, ref: 'User', required: true },    // Give "youtuber" role to the owner in "OrganizationMember" schema
   subscriptions: [{ type: Schema.Types.ObjectId, ref: 'Subscription' }]
 });
