d4ab37d4d2c8da644e9580dbc5e03cc02c9df469
coderRaj07 <12131501.edu@gmail.com>
2024-08-03 18:15:15 +0530

    added s3 upload system

--- routes/videoUpload.js
+++ routes/videoUpload.js
@@ -2,12 +2,12 @@
 
 const express = require('express');
 const { google } = require('googleapis');
-const { S3 } = require('aws-sdk');
+const { S3Client } = require('@aws-sdk/client-s3');
+const { createPresignedPost } = require('@aws-sdk/s3-presigned-post');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+const { v4: uuidv4 } = require('uuid'); 
 
 const router = express.Router();
-
-// Initialize AWS SDK
-const s3 = new S3();
 
 // Initialize YouTube Data API client
 const youtube = google.youtube({
@@ -63,5 +63,45 @@ router.post('/upload', async (req, res) => {
     res.status(500).json({ error: 'An error occurred while uploading the video to YouTube' });
   }
 });
+
+
+  // Upload video to S3
+  const s3Client = new S3Client({
+      credentials: {
+          "accessKeyId": "",
+          "secretAccessKey": ""
+      }
+  })
+
+  router.get("/presignedUrl", authMiddleware, async (req, res) => {
+    try {
+      // Assuming orgId is set correctly in authMiddleware
+      const orgId = req.orgId;
+      const title = req.query.title || 'Untitled'; // Assuming title is passed as a query parameter
+      const description = req.query.description || 'No description'; // Assuming description is passed as a query parameter
+  
+      const { url, fields } = await createPresignedPost(s3Client, {
+        Bucket: "s3OrgVideos",
+        Key: `s3OrgVideos-folder/${orgId}/${uuidv4()}/video.mp4`, // Use uuid for unique key
+        Conditions: [
+          ['content-length-range', 0, 2048 * 1024 * 1024] // 2 GB max
+        ],
+        Fields: {
+          success_action_status: '201',
+          'Content-Type': 'video/mp4',
+          'x-amz-meta-title': title, // Custom metadata for title
+          'x-amz-meta-description': description // Custom metadata for description
+        },
+        Expires: 3600
+      });
+  
+      console.log({ preSignedUrl: url, fields });
+      res.send({ preSignedUrl: url, fields });
+    }
+    catch (error) {
+      console.error("Error creating pre-signed URL", error);
+      res.status(500).send({ error: "Failed to create pre-signed URL" });
+    }
+  })
 
 module.exports = router;
